052a7d4c6332f1dc3e2dc41174f4044135eb49e0
Thomas Yoho <TYohoJr@Gmail.com>
2018-04-20 11:45:11 -0600

    added inches clarification

--- src/PricingForm/PricingForm.js
+++ src/PricingForm/PricingForm.js
@@ -365,9 +365,9 @@ class PricingForm extends React.Component {
             <div>
                 <Form id="pricing-form">
                     <FormGroup>
-                        <Label className="label-float" for="servings">Total Cake Height</Label>
+                        <Label className="label-float" for="servings">Total Cake Height (inches)</Label>
                         <ServingsModal />
-                        <Input disabled type="text" name="servings" id="servings" value={`Total Height: ${this.props.cakeSizeReducer.totalCakeHeight}`} placeholder="Total Cake Size (use calculator above)" />
+                        <Input disabled type="text" name="servings" id="servings" value={`Total Height (inches): ${this.props.cakeSizeReducer.totalCakeHeight}`} placeholder="Total Cake Size (use calculator above)" />
                     </FormGroup>
                     <FormGroup>
                         <Label for="flavor">Flavor</Label>
